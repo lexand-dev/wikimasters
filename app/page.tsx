8f25bc3817b7627aaf05412e7d2704d05a3273bf
alexander flores <alexfloresrayme@gmail.com>
2026-07-26 12:40:48 -0500

    refactor(wiki): drop duplicate mapping on landing page now that getArticles returns ArticleSummary

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,10 @@ import { getArticles } from "@/features/wiki/data/articles";
 export default async function Home() {
   const articles = await getArticles();
 
-  const summaries = articles.map((a) => ({
-    id: a.id,
-    title: a.title,
-    author: a.author,
-    date: a.createdAt,
-    summary: a.excerpt,
-    href: `/wiki/${a.id}`,
-  }));
-
   return (
     <div>
       <main className="max-w-2xl mx-auto mt-10 flex flex-col gap-6">
-        {summaries.map((article) => (
+        {articles.map((article) => (
           <WikiCard key={article.id} article={article} />
         ))}
       </main>
